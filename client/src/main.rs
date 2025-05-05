5868029cf557714d3a73f10cc8201045b7d7b357
rtthw <172864013+rtthw@users.noreply.github.com>
2025-05-05 16:09:33 -0400

    Test custom syscall

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,4 +1,22 @@
 
 
 
-fn main() {}
+fn main() {
+    syscall1(12, 555);
+}
+
+
+
+#[inline]
+pub fn syscall1(n: usize, arg1: usize) -> usize {
+    let mut ret: usize;
+    unsafe { core::arch::asm!(
+        "syscall",
+        inlateout("rax") n => ret,
+        in("rdi") arg1,
+        out("rcx") _, // rcx is used to store old rip
+        out("r11") _, // r11 is used to store old rflags
+        options(nostack, preserves_flags)
+    ) };
+    ret
+}

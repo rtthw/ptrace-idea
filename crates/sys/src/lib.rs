5a071599708383f1aaa70aa89a1fe55d13be6c49
rtthw <172864013+rtthw@users.noreply.github.com>
2025-05-12 06:34:35 -0400

    Add errno support

--- crates/sys/src/lib.rs
+++ crates/sys/src/lib.rs
@@ -11,6 +11,18 @@ pub use call::*;
 
 
 
+unsafe extern "C" {
+    #[link_name = "__errno_location"]
+    pub fn errno_location() -> *mut core::ffi::c_int;
+}
+
+#[inline]
+pub fn errno() -> i32 {
+    unsafe { (*errno_location()) as i32 }
+}
+
+
+
 pub fn exit(status: i32) -> ! {
     call1(60, status as usize);
     unreachable!("Process continued after exiting, this should not be possible")
